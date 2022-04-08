1f4712a6a188132c217dd6be4a85d635a61312c1
hgs-study <hgstudy_@naver.com>
2022-04-08 21:40:44 +0900

    Initial commit

---
+++ src/main/java/com/distributedlockpractice/DistributedLockPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.distributedlockpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLockPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedLockPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/distributedlockpractice/DistributedLockPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedlockpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
