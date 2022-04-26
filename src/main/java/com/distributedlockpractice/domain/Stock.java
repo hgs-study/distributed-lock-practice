3a2ed65d20e00c138f6e06addf5047243783f360
hgs-study <hgstudy_@naver.com>
2022-04-26 19:13:35 +0900

    재고 클래스 활용

--- src/main/java/com/distributedlockpractice/domain/Stock.java
+++ src/main/java/com/distributedlockpractice/domain/Stock.java
@@ -2,11 +2,24 @@ package com.distributedlockpractice.domain;
 
 public class Stock {
     private String name;
+    private String keyId;
+    private int amount;
 
-    private Long amount;
-
-    public Stock(String name, Long amount) {
+    public Stock(String name, String keyId, int amount) {
         this.name = name;
+        this.keyId = keyId;
         this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public int getAmount() {
+        return amount;
     }
 }
--- src/test/java/com/distributedlockpractice/service/StockServiceTest.java
+++ src/test/java/com/distributedlockpractice/service/StockServiceTest.java
@@ -1,5 +1,6 @@
 package com.distributedlockpractice.service;
 
+import com.distributedlockpractice.domain.Stock;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,32 +19,36 @@ class StockServiceTest {
     @Autowired
     private StockService stockService;
     private String stockKey;
+    private Stock peenut;
 
     @BeforeEach
-    void 키_세팅(){
+    void 재고_키_세팅(){
         final String name = "peanut";
-        final String keyId = name + "001";
-        this.stockKey = stockService.keyResolver(name, keyId);
+        final String keyId = "001";
+        final int amount = 100;
+        final Stock peenut = new Stock(name, keyId, amount);
+
+        this.stockKey = stockService.keyResolver(peenut.getName(), peenut.getKeyId());
+        this.peenut = peenut;
+        stockService.setStock(this.stockKey, amount);
     }
 
     @Test
     @Order(1)
-    void 상품_재고_세팅(){
-        final int amount = 10;
-
-        stockService.setStock(this.stockKey, amount);
+    void 상품_수량_확인(){
+        final int amount = this.peenut.getAmount();
 
         final int currentCount = stockService.currentStock(stockKey);
+
         assertEquals(amount, currentCount);
     }
 
     @Test
     @Order(2)
     void 상품_재고_카운트만큼_감소(){
-        final int amount = 10;
+        final int amount = this.peenut.getAmount();
         final int count = 2;
 
-        stockService.setStock(this.stockKey, amount);
         stockService.decrease(this.stockKey, count);
 
         final int currentCount = stockService.currentStock(stockKey);
@@ -53,16 +58,14 @@ class StockServiceTest {
     @Test
     @Order(3)
     void 락X_땅콩_100개를_사람_100명이_2개씩_구매() throws InterruptedException {
-        final int THREAD_LENGTH = 100;
-        final int amount = 100;
+        final int people = 100;
         final int count = 2;
         final int soldOut = 0;
-        final CountDownLatch countDownLatch = new CountDownLatch(THREAD_LENGTH);
-        stockService.setStock(this.stockKey, amount);
+        final CountDownLatch countDownLatch = new CountDownLatch(people);
 
         List<Thread> workers = Stream
                                 .generate(() -> new Thread(new BuyNoLockWorker(this.stockKey, count, countDownLatch)))
-                                .limit(THREAD_LENGTH)
+                                .limit(people)
                                 .collect(Collectors.toList());
         workers.forEach(Thread::start);
         countDownLatch.await();
@@ -74,21 +77,19 @@ class StockServiceTest {
     @Test
     @Order(4)
     void 락O_땅콩_100개를_사람_100명이_2개씩_구매() throws InterruptedException {
-        final int THREAD_LENGTH = 100;
-        final int amount = 100;
+        final int people = 100;
         final int count = 2;
         final int soldOut = 0;
-        final CountDownLatch countDownLatch = new CountDownLatch(THREAD_LENGTH);
-        stockService.setStock(this.stockKey, amount);
+        final CountDownLatch countDownLatch = new CountDownLatch(people);
 
         List<Thread> workers = Stream
                                 .generate(() -> new Thread(new BuyWorker(this.stockKey, count, countDownLatch)))
-                                .limit(THREAD_LENGTH)
+                                .limit(people)
                                 .collect(Collectors.toList());
         workers.forEach(Thread::start);
         countDownLatch.await();
 
-        final int currentCount = stockService.currentStock(stockKey);
+        final int currentCount = stockService.currentStock(this.stockKey);
         assertEquals(soldOut, currentCount);
     }
 
